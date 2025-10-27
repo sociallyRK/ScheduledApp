be5d231cea7b153b73d26cd93ae6a03014a75cac
sociallyrk <sociallyrk@gmail.com>
2025-10-26 18:43:30 -0700

    RN076 fix metro and minimal host

--- android/app/src/main/java/com/rahulkhanna0328/scheduled/MainApplication.kt
+++ android/app/src/main/java/com/rahulkhanna0328/scheduled/MainApplication.kt
@@ -2,20 +2,16 @@ package com.rahulkhanna0328.scheduled
 import android.app.Application
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
 import com.facebook.soloader.SoLoader
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.rahulkhanna0328.scheduled.BuildConfig
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage
 
 class MainApplication : Application(), ReactApplication {
-  private val host = object : DefaultReactNativeHost(this) {
+  override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
     override fun getUseDeveloperSupport() = BuildConfig.DEBUG
-    override fun getPackages(): List<ReactPackage> = listOf(AsyncStoragePackage())
     override fun getJSMainModuleName() = "index"
     override val isNewArchEnabled = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
     override val isHermesEnabled = BuildConfig.IS_HERMES_ENABLED
   }
-  override val reactNativeHost: ReactNativeHost get() = host
   override fun onCreate() { super.onCreate(); SoLoader.init(this, false) }
 }
