d1a0dba55ee2fb56ba70f777bbb89cc557fcce56
sociallyrk <sociallyrk@gmail.com>
2025-10-26 18:49:06 -0700

    RN076: implement getPackages minimal host

--- android/app/src/main/java/com/rahulkhanna0328/scheduled/MainApplication.kt
+++ android/app/src/main/java/com/rahulkhanna0328/scheduled/MainApplication.kt
@@ -2,14 +2,16 @@ package com.rahulkhanna0328.scheduled
 import android.app.Application
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
-import com.facebook.soloader.SoLoader
+import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
 import com.rahulkhanna0328.scheduled.BuildConfig
 
 class MainApplication : Application(), ReactApplication {
   override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
     override fun getUseDeveloperSupport() = BuildConfig.DEBUG
     override fun getJSMainModuleName() = "index"
+    override fun getPackages(): List<ReactPackage> = emptyList()
     override val isNewArchEnabled = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
     override val isHermesEnabled = BuildConfig.IS_HERMES_ENABLED
   }
